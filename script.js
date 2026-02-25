432d5880b7a57f336ff341dff9e605eaf4dd0df6
Stucanovsky Max <hekkouni@gmail.com>
2026-02-25 13:36:56 +0300

    update + add html and css file

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-window.onload = function(){ 
-    // Переменные для хранения чисел и операций
-    let a = ''           // Первое число
-    let b = ''           // Второе число
-    let expressionResult = ''  // Результат вычисления
-    let selectedOperation = null  // Выбранная операция
-        // Получаем доступ к экрану калькулятора в поле вывода
-    const outputElement = document.getElementById("result")
+window.onload = function() { 
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null 
+    
 
-    // Получаем все кнопки с цифрами (их id начинаются с "btn_digit_")
+    const outputElement = document.getElementById("result")
     const digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
 
     function onDigitButtonClicked(digit) {
-        // Если операция не выбрана, работаем с первым числом (a) - после выбора операции начинается ввод второго числа
         if (!selectedOperation) {
-            // Проверяем, не пытаемся ли мы добавить вторую точку
             if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
-                // здесь у нас происходит складывание сохраненного уже числа и нажатой цифры. Оба поля string, поэтому
-                // каждый раз цифра записывается в конец строки. Например: a = '14', digit = '5', 
-                // a += digit - это короткая запись a = a + digit - поэтомоу после этой операции a = '145'
                 a += digit;
             }
             outputElement.innerHTML = a;
         } 
-        // Если операция выбрана, работаем со вторым числом (b)
         else {
             if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
                 b += digit;
@@ -31,10 +23,8 @@ window.onload = function(){
         }
     }
 
-        // Настраиваем обработчики для цифровых кнопок - для каждой кнопки с цифрой и точкой вызываем выше написанную функцию по формированию числа
     digitButtons.forEach(button => {
         button.onclick = function() {
-            // берем текст, написанный на кнопке - он и является цифрой
             const digitValue = button.innerHTML;
             onDigitButtonClicked(digitValue);
         }
@@ -64,7 +54,6 @@ window.onload = function(){
         b = '';
     }
 
-    // Настраиваем обработчики для кнопок операций - сохраняем выбранную операцию в ранее созданную переменную selectedOperation
     document.getElementById("btn_op_mult").onclick = function() { 
         if (a === '') return;
         cumulative();
@@ -102,7 +91,7 @@ window.onload = function(){
         }
         else {
             b /= 100;
-            outputElement.innerHTML = b;
+            outputElement.innerHTML = b;              
         }
     }
     document.getElementById("btn_op_backspace").onclick = function() {
@@ -188,19 +177,20 @@ window.onload = function(){
             outputElement.innerHTML = b;
         }
     }
-    flag = true;
+    flag_1 = true;
     document.getElementById("btn_change_theme").onclick = function() {
-        flag = !flag;
-        if (flag) {
+        flag_1 = !flag_1;
+        if (flag_1) {
             document.body.style.backgroundColor = 'black';
         }
         else {
             document.body.style.backgroundColor = 'white';
         }
     }
+    flag_2 = true;
     document.getElementById("btn_change_res_theme").onclick = function() {
-        flag = !flag;
-        if (flag) {
+        flag_2 = !flag_2;
+        if (flag_2) {
             document.getElementById("result").style.backgroundColor = 'white';
             document.getElementById("result").style.color = 'black';
         }
@@ -209,8 +199,7 @@ window.onload = function(){
             document.getElementById("result").style.color = 'white';
         }
     }
- 
-        // Очищаем все значения при нажатии на кнопку C (вешаем обработчик события click на кнопку С)
+
     document.getElementById("btn_op_clear").onclick = function() { 
         a = ''
         b = ''
@@ -218,17 +207,13 @@ window.onload = function(){
         expressionResult = ''
         outputElement.innerHTML = 0
     }
-        // Вычисляем результат при нажатии на = (вешаем обработчик события click на кнопку =)
+
     document.getElementById("btn_op_equal").onclick = function() { 
-        // Проверяем, что у нас есть оба числа и операция
         if (a === '' || b === '' || !selectedOperation)
             return
-            
-        // Выполняем выбранную операцию - чтобы не плодить if, воспользуемся удобной и более наглядной функцией сравнения switch, которая на основе значения переданной переменной выполняет нужный кейс. В case указывается ожидаемое точное значение переменной (это может быть любое значение), а затем после : пишется код, который нужно выполнить в данном случае. Case проверяются последовательно, выход из switch происходит при попадании на break или если значение не совпало ни с чем.
         switch(selectedOperation) { 
             case 'x':
                 expressionResult = (+a) * (+b)
-                // обязательно пишется в конце действий case, чтобы выйти из switch, иначе продолжится сравнение case дальше
                 break;
             case '+':
                 expressionResult = (+a) + (+b)
@@ -239,17 +224,14 @@ window.onload = function(){
             case '/':
                 expressionResult = (+a) / (+b)
                 break;
-            // желательно (но не обязательно) всегда прописывать дефолтное поведение, в случае если в переменной окажется не перечисленное выше значение. в нашем случае это не нужно.
             default:
                 break;
         }
         
-        // Сохраняем результат и очищаем второе число, чтобы при новом вводе записывать значение нового числа в b
         a = expressionResult.toString()
         b = ''
         selectedOperation = null
 
-        // Показываем результат на экране
         outputElement.innerHTML = a
     }
 };
